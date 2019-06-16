91439cf36d47119e9679db1570f96d431ff8760a
Anthony Morrison <amorri45@emich.edu>
2019-06-16 15:37:04 -0400

    sign in sign out and sign up functioning properly

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -14,23 +14,24 @@ import AccountPage from '../Account';
 import AdminPage from '../Admin';
 
 import * as ROUTES from '../../constants/routes';
+import { withAuthentication } from '../Session';
 
 const App = () => (
   <Router>
     <div>
       <Navigation />
-
       <hr />
-
-      <Route exact path={ROUTES.LANDING} component={LandingPage} />
-      <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
-      <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
-      <Route exact path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-      <Route exact path={ROUTES.HOME} component={HomePage} />
-      <Route exact path={ROUTES.ACCOUNT} component={AccountPage} />
-      <Route exact path={ROUTES.ADMIN} component={AdminPage} />
+        <Route exact path={ROUTES.LANDING} component={LandingPage} />
+        <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
+        <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
+        <Route exact path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+        <Route exact path={ROUTES.HOME} component={HomePage} />
+        <Route exact path={ROUTES.ACCOUNT} component={AccountPage} />
+        <Route exact path={ROUTES.ADMIN} component={AdminPage} />
     </div>
   </Router>
 );
 
-export default App;
+
+
+export default withAuthentication(App);
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,13 +3,20 @@ import { Link } from 'react-router-dom';
 
 import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
+import { AuthUserContext } from '../Session';
 
-const Navigation = () => (
+const Navigation = ({ authUser }) => (
   <div>
+    <AuthUserContext.Consumer>
+      {authUser =>
+        authUser? <NavigationAuth /> : <NavigationNonAuth />
+      }
+    </AuthUserContext.Consumer>
+  </div>
+);
+
+const NavigationAuth = () => (
     <ul>
-      <li>
-        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-      </li>
       <li>
         <Link to={ROUTES.LANDING}>Landing</Link>
       </li>
@@ -20,13 +27,20 @@ const Navigation = () => (
         <Link to={ROUTES.ACCOUNT}>Account</Link>
       </li>
       <li>
-        <Link to={ROUTES.ADMIN}>Admin</Link>
-      </li>
-      <li>
         <SignOutButton />
       </li>
     </ul>
-  </div>
+);
+
+const NavigationNonAuth = () => (
+  <ul>
+    <li>
+      <Link to={ROUTES.LANDING}>Landing</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+    </li>
+  </ul>
 );
 
 export default Navigation;
